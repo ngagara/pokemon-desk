2752746c5e047819f2b1cc1f8408848de90cc5e1
Nikolay Gagara <ggn.f763@gmail.com>
2021-05-13 21:11:38 +0300

    homework-2

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import cn from 'classnames';
 import style from './App.module.scss';
 
 const App = () => {
-  //   console.log(`123es`);
-
   return <div className={cn(style.header)}>This App</div>;
 };
 
