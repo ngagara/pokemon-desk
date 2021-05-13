77abbbae72cd0879a8c2ae0d90c907675451a5bd
Nikolay Gagara <ggn.f763@gmail.com>
2021-05-13 19:59:32 +0300

    new commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import cn from 'classnames';
+import style from './App.module.scss';
+
+const App = () => {
+  //   console.log(`123es`);
+
+  return <div className={cn(style.header)}>This App</div>;
+};
+
+export default App;
